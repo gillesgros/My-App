f9939f0e195f8e40da4a4a6af90e31481fd82750
Gros Gilles <gilles@71yeti.fr>
2024-09-12 17:24:31 +0200

    Mise à jour pour deploiement

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig( ({ mode}) => {
+  
+  return {
+    plugins: [
+      { name: 'add-datetime',
+        transformIndexHtml(html) {
+          const datetime = new Date().toISOString();
+          console.log('\nGenerate Date and Time: ', datetime);
+          return html.replace(/id="datetime">/, `id="datetime">${datetime}`);
+        }
+      },
+      vue()
+    ],
   base: "/My-App"
+}
 })
