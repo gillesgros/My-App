fb994b4680c3b75e448be3db30646691db2f4eaf
Gros Gilles <gilles@71yeti.fr>
2024-09-16 12:33:35 +0200

    fix date genartion

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig( ({ mode}) => {
     plugins: [
       { name: 'add-datetime',
         transformIndexHtml(html) {
-          const datetime = new Date().toLocaleString();
+          const datetime = new Date().toLocaleString('fr-FR');
           console.log('\nGenerate Date and Time: ', datetime);
           return html.replace(/id="datetime">/g, `id="datetime">${datetime}`);
         }
