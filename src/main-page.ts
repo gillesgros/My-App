2481d06441324139b38e4a6593f8fa0813282679
Gros Gilles <gilles@71yeti.fr>
2024-09-17 06:17:33 +0200

    rules

--- src/main-page.ts
+++ src/main-page.ts
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Index from './views/Index.vue'
+/* import Sets from './views/Sets.vue'
+import Rules from './views/Rules.vue'
+import Boxes from './views/Boxes.vue'
+import Cards from './views/Cards.vue'
+import Settings from './views/Settings.vue' */
 
-createApp(App).mount('#app')
+import { initialize } from './setup';
+import { AppCreateRouterMultiple } from './router';
+
+import '../styles/index.styl';
+import '../styles/sets.styl';
+import '../styles/rules.styl';
+import '../styles/cards.styl';
+
+
+initialize(AppCreateRouterMultiple([
+                { paths: ["/index.html", "/index", "/", ""], component : Index },
+/*                 { paths: ["/sets"], component : Sets },
+                { paths: ["/rules"], component : Rules },
+                { paths: ["/boxes"], component : Boxes },
+                { paths: ["/cards"], component : Cards },
+                { paths: ["/settings"], component : Settings }, */
+            ]))
